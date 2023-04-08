f187b85890bfbc22144e0b890de8658f1defc426
LoveLolita <q781934731@gmail.com>
2023-04-08 21:59:20 +0800

    Remove ignored files

--- src/pages/flight/list/list.jsx
+++ src/pages/flight/list/list.jsx
@@ -1,4 +1,4 @@
-import { PureComponent } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {
   View,
   ScrollView,
@@ -11,30 +11,18 @@ import Taro, { getCurrentInstance } from "@tarojs/taro";
 import dayjs from "dayjs";
 import { weekDay } from "@/common/utils";
 import { MIN_DATE, MAX_DATE, ERR_MES } from "@/common/constant";
-import Skeleton from 'taro-skeleton'
+import Skeleton from "taro-skeleton";
 import { flightListReq } from "@/common/api";
 import tools from "@/common/tools";
 // import VirtualList from '@/components/VirtualList'
 
-
-import 'taro-skeleton/dist/index.css'
+import "taro-skeleton/dist/index.css";
 
 import "./list.scss";
 
-export default class List extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dateList: this.formatDateList(),
-      flightData: {}, // 航班参数
-      flightList: [], // 航班列表
-      flightCompanyList: [], // 航司列表
-      curAirCompanyIndex: "", // 当前选中的下标
-      scrollTop: "",
-    };
-    this.initFlightList = [];
-  }
-  componentDidMount() {
+const List = () => {
+  /* 切换标题 */
+  useEffect(() => {
     const { params } = getCurrentInstance().router;
     const {
       dptCityId,
@@ -44,9 +32,17 @@ export default class List extends PureComponent {
       dptDate,
       arrAirportName,
       dptAirportName,
-    } = params
-    this.setState({
-      flightData: {
+    } = params;
+    console.log(params);
+
+    // 修改小程序标题
+    Taro.setNavigationBarTitle({
+      title: `${dptCityName} - ${arrCityName}`,
+    });
+
+    // 储存query信息
+    setFlightData(() => {
+      return {
         dptCityId,
         dptCityName,
         arrCityId,
@@ -54,38 +50,57 @@ export default class List extends PureComponent {
         dptDate,
         arrAirportName,
         dptAirportName,
-      }
-    }, this.getList)
-    Taro.setNavigationBarTitle({
-      title: `${dptCityName} - ${arrCityName}`
-    })
-  }
-  getList = () => {
-    const { flightData } = this.state
+      };
+    });
+  }, []);
+  /* 切换标题end */
+
+  /* 航班列表 */
+  const [flightData, setFlightData] = useState({}); // 航班参数
+  const [scrollTop, setScrollTop] = useState("");
+  const [flightList, setFlightList] = useState([]); // 航班列表
+  const [flightCompanyList, setFlightCompanyList] = useState([]); // 航司列表
+  const [initFlightList, setInitFlightList] = useState([]);
+  const [dateList, setDateList] = useState([]);
+  useEffect(() => {
+    setDateList(formatDateList());
+  }, []);
+
+  // 获取航班列表
+  const getList = () => {
     tools.showLoading();
-    this.setState({
+    setScrollTop({
       scrollTop: "",
-    })
+    });
     flightListReq({
-      ...flightData
+      ...flightData,
     })
-      .then(res => {
-        const companyArr = res.result?.map(item => item.airCompanyName)
-        this.setState({
-          flightList: res.result,
-          flightCompanyList: [...new Set(companyArr)],
-          scrollTop: 0,
-        })
-        this.initFlightList = res.result
+      .then((res) => {
+        console.log(res);
+        const companyArr = res.result?.map((item) => {
+          return item.airCompanyName;
+        });
+        setFlightList(res.result);
+        setFlightCompanyList([...new Set(companyArr)]);
+        setScrollTop(0);
+        setInitFlightList(res.result);
       })
       .catch(() => {
-        tools.showToast(ERR_MES)
+        tools.showToast(ERR_MES);
       })
       .finally(() => {
-        tools.hideLoading()
-      })
-  }
-  formatDateList = () => {
+        tools.hideLoading();
+      });
+  };
+
+  // 拿到quey值后请求航空数据
+  useEffect(() => {
+    getList();
+  }, [flightData]);
+
+
+  // 处理列表时间数据
+  const formatDateList = () => {
     let minStr = dayjs(MIN_DATE).valueOf();
     const maxStr = dayjs(MAX_DATE).valueOf();
     const dayStr = 1000 * 60 * 60 * 24; // 一天
@@ -99,41 +114,47 @@ export default class List extends PureComponent {
     }
     return res;
   };
-  onAirCompanyChange = (e) => {
+  /* 航班列表end */
+
+  /*  */
+  const [curAirCompanyIndex, setCurAirCompanyIndex] = useState(""); // 当前选中的下标
+
+  const onAirCompanyChange = (e) => {
     const { value } = e.detail;
-    const { flightCompanyList } = this.state
-    this.setState({
-      curAirCompanyIndex: value,
-      scrollTop: "",
-    }, () => {
-      const res = this.initFlightList.filter(item => item.airCompanyName === flightCompanyList[value])
-      this.setState({
-        flightList: res,
-        scrollTop: 0,
-      })
-    })
-  }
-  chooseDate = (date) => {
-    this.setState({
-      flightData: {
-        ...this.state.flightData,
-        dptDate: date,
-      }
-    }, this.getList)
-  }
+    setCurAirCompanyIndex(value);
+    setScrollTop("");
+    const res = initFlightList.filter(
+      (item) => item.airCompanyName === flightCompanyList[value]
+    );
+    setFlightList(res);
+    setScrollTop(0);
+  };
+  // 切换时间
+  const chooseDate = (date) => {
+    setFlightData({
+      ...flightData,
+      dptDate: date,
+    });
+    getList();
+  };
+  /*  */
+
   /**
    * 跳转详情页
-   * @{param}	 
+   * @{param}
    */
-  onFlightClick = (curFlight) => {
+  const onFlightClick = (curFlight) => {
     tools.navigateTo({
-      url: '/pages/flight/detail/detail',
+      url: "/pages/flight/detail/detail",
       data: {
-        ...curFlight
-      }
-    })
-  }
-  handleRender = (flight, index) => {
+        ...curFlight,
+      },
+    });
+  };
+  /* 跳转详情页end */
+
+  /*  */
+  const handleRender = (flight, index) => {
     const {
       dptAirportName,
       dptTimeStr,
@@ -145,39 +166,31 @@ export default class List extends PureComponent {
     } = flight;
     return (
       <Block key={flight.id}>
-        {
-          index === 3 && (
-            <View className='notice'>
-              <Image className='notice-logo' src='https://images3.c-ctrip.com/ztrip/xiaochengxu/shangzhang_zx.png'></Image>
-              <Text className='notice-text'>价格可能会上涨，建议尽快预定</Text>
-            </View>  
-          )
-        }
-        <View
-          className='list-item'
-          onClick={() => this.onFlightClick(flight)}
-        >
+        {index === 3 && (
+          <View className='notice'>
+            <Image
+              className='notice-logo'
+              src='https://images3.c-ctrip.com/ztrip/xiaochengxu/shangzhang_zx.png'
+            ></Image>
+            <Text className='notice-text'>价格可能会上涨，建议尽快预定</Text>
+          </View>
+        )}
+        <View className='list-item' onClick={() => this.onFlightClick(flight)}>
           <View className='item-price'>
             <View className='flight-row'>
               <View className='depart'>
                 <Text className='flight-time'>{dptTimeStr}</Text>
-                <Text className='airport-name'>
-                  {dptAirportName}
-                </Text>
+                <Text className='airport-name'>{dptAirportName}</Text>
               </View>
               <View className='separator'>
                 <View className='spt-arr'></View>
               </View>
               <View className='arrival'>
                 <Text className='flight-time'>{arrTimeStr}</Text>
-                <Text className='airport-name'>
-                  {arrAirportName}
-                </Text>
+                <Text className='airport-name'>{arrAirportName}</Text>
               </View>
             </View>
-            <Text className='flight-price color-red'>
-                          ¥ {price}
-            </Text>
+            <Text className='flight-price color-red'>¥ {price}</Text>
           </View>
           <View className='air-info'>
             <Image className='logo' src={airIcon} />
@@ -185,131 +198,117 @@ export default class List extends PureComponent {
           </View>
         </View>
       </Block>
-    )
-    
-  }
-  render() {
-    const {
-      dateList,
-      flightData,
-      flightList,
-      flightCompanyList,
-      curAirCompanyIndex,
-      scrollTop
-    } = this.state;
-    const { dptDate } = flightData
-    return (
-      <View className='list-container'>
-        <View className='calendar-list'>
+    );
+  };
+  /*  */
+
+  return (
+    <View className='list-container'>
+      <View className='calendar-list'>
+        <ScrollView
+          className='calendar-scroll-list'
+          scrollX
+          scrollWithAnimation
+          scrollIntoView={`date-${flightData.dptDate}`}
+        >
+          {dateList.map((date) => {
+            return (
+              <View
+                key={date.dateStr}
+                className={`item ${
+                  date.dateStr === flightData.dptDate ? "cur" : ""
+                }`}
+                id={`date-${date.dateStr}`}
+                onClick={() => chooseDate(date.dateStr)}
+              >
+                <View className='date'>{date.day}</View>
+                <View className='week'>{date.week}</View>
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+      {flightList.length ? (
+        <View id='flight-list'>
+          {/* 性能优化篇：虚拟列表 */}
+          {/* <VirtualList className="flight-scroll-list" list={flightList} onRender={this.handleRender}></VirtualList> */}
           <ScrollView
-            className='calendar-scroll-list'
-            scrollX
-            scrollWithAnimation
-            scrollIntoView={`date-${dptDate}`}
+            className='flight-scroll-list'
+            scrollY
+            scrollTop={scrollTop}
           >
-            {dateList.map((date) => {
+            {flightList?.map((flight, index) => {
+              const {
+                dptAirportName,
+                dptTimeStr,
+                arrTimeStr,
+                arrAirportName,
+                airIcon,
+                airCompanyName,
+                price,
+              } = flight;
               return (
-                <View
-                  key={date.dateStr}
-                  className={`item ${
-                    date.dateStr === dptDate ? "cur" : ""
-                  }`}
-                  id={`date-${date.dateStr}`}
-                  onClick={() => this.chooseDate(date.dateStr)}
-                >
-                  <View className='date'>{date.day}</View>
-                  <View className='week'>{date.week}</View>
-                </View>
+                <Block key={flight.id}>
+                  {index === 3 && (
+                    <View className='notice'>
+                      <Image
+                        className='notice-logo'
+                        src='https://i.postimg.cc/dhGPDTjq/2.png'
+                      ></Image>
+                      <Text className='notice-text'>
+                        价格可能会上涨，建议尽快预定
+                      </Text>
+                    </View>
+                  )}
+                  <View
+                    className='list-item'
+                    onClick={() => onFlightClick(flight)}
+                  >
+                    <View className='item-price'>
+                      <View className='flight-row'>
+                        <View className='depart'>
+                          <Text className='flight-time'>{dptTimeStr}</Text>
+                          <Text className='airport-name'>{dptAirportName}</Text>
+                        </View>
+                        <View className='separator'>
+                          <View className='spt-arr'></View>
+                        </View>
+                        <View className='arrival'>
+                          <Text className='flight-time'>{arrTimeStr}</Text>
+                          <Text className='airport-name'>{arrAirportName}</Text>
+                        </View>
+                      </View>
+                      <Text className='flight-price color-red'>¥ {price}</Text>
+                    </View>
+                    <View className='air-info'>
+                      <Image className='logo' src={airIcon} />
+                      <Text className='company-name'>{airCompanyName}</Text>
+                    </View>
+                  </View>
+                </Block>
               );
             })}
           </ScrollView>
         </View>
-        {flightList.length ? (
-          <View
-            id='flight-list'
-          >
-            {/* 性能优化篇：虚拟列表 */}
-            {/* <VirtualList className="flight-scroll-list" list={flightList} onRender={this.handleRender}></VirtualList> */}
-            <ScrollView
-              className='flight-scroll-list'
-              scrollY
-              scrollTop={scrollTop}
-            >
-              {flightList?.map((flight, index) => {
-                const {
-                  dptAirportName,
-                  dptTimeStr,
-                  arrTimeStr,
-                  arrAirportName,
-                  airIcon,
-                  airCompanyName,
-                  price,
-                } = flight;
-                return (
-                  <Block key={flight.id}>
-                    {
-                      index === 3 && (
-                        <View className='notice'>
-                          <Image className='notice-logo' src='https://i.postimg.cc/dhGPDTjq/2.png'></Image>
-                          <Text className='notice-text'>价格可能会上涨，建议尽快预定</Text>
-                        </View>  
-                      )
-                    }
-                    <View
-                      className='list-item'
-                      onClick={() => this.onFlightClick(flight)}
-                    >
-                      <View className='item-price'>
-                        <View className='flight-row'>
-                          <View className='depart'>
-                            <Text className='flight-time'>{dptTimeStr}</Text>
-                            <Text className='airport-name'>
-                              {dptAirportName}
-                            </Text>
-                          </View>
-                          <View className='separator'>
-                            <View className='spt-arr'></View>
-                          </View>
-                          <View className='arrival'>
-                            <Text className='flight-time'>{arrTimeStr}</Text>
-                            <Text className='airport-name'>
-                              {arrAirportName}
-                            </Text>
-                          </View>
-                        </View>
-                        <Text className='flight-price color-red'>
-                          ¥ {price}
-                        </Text>
-                      </View>
-                      <View className='air-info'>
-                        <Image className='logo' src={airIcon} />
-                        <Text className='company-name'>{airCompanyName}</Text>
-                      </View>
-                    </View>
-                  </Block>
-                );
-              })}
-            </ScrollView>
-          </View>
-        ) : (
-          <View className='skeleton-box'>
-            {Array(7)
-              .fill(0)
-              .map((item, index) => {
-                return <Skeleton key={index} row={3} action rowHeight={34} />;
-              })}
-          </View>
-        )}
-        <View className={`flilter-btn ${flightList?.length ? "" : "hidden"}`}>
-          <Picker
-            range={flightCompanyList}
-            value={curAirCompanyIndex}
-            onChange={this.onAirCompanyChange}
-          >
-            筛选
-          </Picker>
+      ) : (
+        <View className='skeleton-box'>
+          {Array(7)
+            .fill(0)
+            .map((item, index) => {
+              return <Skeleton key={index} row={3} action rowHeight={34} />;
+            })}
         </View>
+      )}
+      <View className={`flilter-btn ${flightList?.length ? "" : "hidden"}`}>
+        <Picker
+          range={flightCompanyList}
+          value={curAirCompanyIndex}
+          onChange={onAirCompanyChange}
+        >
+          筛选
+        </Picker>
       </View>
-    )
-  }
-}
+    </View>
+  );
+};
+export default List;
